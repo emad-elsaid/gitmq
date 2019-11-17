dd391069b1de527cd16cd0a7bc6447cb0cb3b9d5
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2019-11-17 20:39:57 +0100

    remove the logger

--- lib/config.rb
+++ lib/config.rb
@@ -1,20 +1,16 @@
 # frozen_string_literal: true
 
-require 'logger'
 require 'forwardable'
 require 'ostruct'
 
 module GitMQ
   class Config
     ATTRS = %i[
-      logger
       storage
       id
     ].freeze
 
-    DEFAULTS = {
-      logger: Logger.new(STDOUT)
-    }.freeze
+    DEFAULTS = {}.freeze
 
     extend Forwardable
     def_instance_delegators :@conf, *ATTRS
